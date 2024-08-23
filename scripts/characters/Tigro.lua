76f7f6c58fbf123e8a74c5310372741a10b9a1be
KOSMOGOR <mortalmortal2015@mail.ru>
2024-08-24 00:06:56 +0300

    add: Tigro tail add: hunter chest sprites update: close #1 update: hunter key and chains sounds fix: empty items locking fix: hunter key spawns

--- scripts/characters/Tigro.lua
+++ scripts/characters/Tigro.lua
@@ -4,6 +4,7 @@ local callbacks = {}
 mod.PLAYER_TIGRO = Isaac.GetPlayerTypeByName("Tigro")
 local HunterKeyVariant = Isaac.GetEntityVariantByName("Hunter Key")
 local HunterKeyPartVariant = Isaac.GetEntityVariantByName("Hunter Key Part")
+local KeyPriceVariant = Isaac.GetEntityVariantByName("Key Price")
 local RenderSincePickup = 0
 
 function callbacks:SetDefaultValues(player) -- Set Defaul Values
@@ -20,6 +21,7 @@ mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, callbacks.SetDefaultValues)
 
 mod.Characters[mod.PLAYER_TIGRO] = {
     Hair = Isaac.GetCostumeIdByPath("gfx/characters/tigro_hair.anm2"),
+    Tail = Isaac.GetCostumeIdByPath("gfx/characters/tigro_tail.anm2"),
     DAMAGE = 0,
     DAMAGE_MULT = 1,
     FIREDELAY = 1,
@@ -36,6 +38,7 @@ mod.Characters[mod.PLAYER_TIGRO] = {
 function callbacks:OnInit(player)
     if player:GetPlayerType() == mod.PLAYER_TIGRO then
         player:AddNullCostume(mod.Characters[mod.PLAYER_TIGRO].Hair)
+        player:AddNullCostume(mod.Characters[mod.PLAYER_TIGRO].Tail)
     end
 end
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, callbacks.OnInit)
@@ -44,14 +47,12 @@ local ItemChainsVarian = Isaac.GetEntityVariantByName("ItemChains")
 local function LockItemSprite(item)
     local ind = item.InitSeed
     local effect1 = Isaac.Spawn(1000, ItemChainsVarian, 0, item.Position, Vector.Zero, nil):ToEffect()
-    effect1:GetSprite():SetFrame("Front", 0)
+    effect1:GetSprite():Play("Front", true)
     effect1.DepthOffset = 5
-    effect1.SpriteOffset = Vector(0, -19)
     effect1:FollowParent(item)
     local effect2 = Isaac.Spawn(1000, ItemChainsVarian, 0, item.Position, Vector.Zero, nil):ToEffect()
-    effect2:GetSprite():SetFrame("Back", 0)
+    effect2:GetSprite():Play("Back", true)
     effect2.DepthOffset = -5
-    effect2.SpriteOffset = Vector(0, -19)
     effect2:FollowParent(item)
     item.Child = effect1
     item.Child.Child = effect2
@@ -60,7 +61,7 @@ end
 function callbacks:OnPickupInit(pickup)
     if not mod.CharaterInGame(mod.PLAYER_TIGRO) then return end
     local ind = pickup.InitSeed
-    if pickup.Variant == 100 and not mod.Data.Tigro.checkedItems[ind] then
+    if pickup.Variant == 100 and not mod.Data.Tigro.checkedItems[ind] and pickup:GetSprite():GetAnimation() ~= "Empty" then
         mod.Data.Tigro.checkedItems[ind] = true
         local cost = 4
         if pickup.Price ~= 0 then
@@ -101,6 +102,7 @@ function callbacks:LockedItemInteraction(pickup, collider, low)
             mod.Data.Tigro.keyShards = mod.Data.Tigro.keyShards - mod.Data.Tigro.lockedItems[ind].cost
             pickup.Child:GetSprite():Play("FrontUnlocking")
             pickup.Child.Child:GetSprite():Play("BackUnlocking")
+            SFXManager():Play(SoundEffect.SOUND_CHAIN_BREAK)
         end
         return false
     end
@@ -125,7 +127,7 @@ function callbacks:GrabHunterKey(pickup, collider, low)
         pickup:GetSprite():Play("Collect")
         mod.Data.Tigro.keyShards = mod.Data.Tigro.keyShards + mod._if(pickup.Variant == HunterKeyVariant, 4, 1)
         SFXManager():Play(
-            mod._if(pickup.Variant == HunterKeyVariant or mod.Data.Tigro.keyShards % 4 == 0, SoundEffect.SOUND_GOLDENKEY, SoundEffect.SOUND_BONE_HEART)
+            mod._if(pickup.Variant == HunterKeyVariant or mod.Data.Tigro.keyShards % 4 == 0, SoundEffect.SOUND_DEATH_BURST_BONE, SoundEffect.SOUND_BONE_HEART)
         )
         RenderSincePickup = 0
         return true
@@ -147,16 +149,17 @@ function callbacks:HunterKeyUpdate(pickup)
 end
 mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, callbacks.HunterKeyUpdate)
 
-function callbacks:SpawnHunterKey(pickup, variant, subtype)
-    if variant ~= 30 then return end
+function callbacks:SpawnHunterKey(pickup)
+    if not pickup:GetSprite():IsPlaying("Appear") or pickup:GetSprite():GetFrame() ~= 1 then return end
+    if pickup.Variant ~= 30 then return end
     local r = mod.rand(1, 1000)
     if r <= 5 then
-        return {HunterKeyVariant, 0}
+        pickup:Morph(5, HunterKeyVariant, 0, true, false, true)
     elseif r <= 250 + 5 then
-        return {HunterKeyPartVariant, 0}
+        pickup:Morph(5, HunterKeyPartVariant, 0, true, false, true)
     end
 end
-mod:AddCallback(ModCallbacks.MC_POST_PICKUP_SELECTION, callbacks.SpawnHunterKey)
+mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, callbacks.SpawnHunterKey)
 
 local function IsActionHold(action)
     for i = 0, Game():GetNumPlayers() - 1 do
@@ -209,5 +212,5 @@ mod:AddCallback(ModCallbacks.MC_POST_RENDER, callbacks.RenderDreamBookCharges)
 
 
 mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, function(_, pickup)
-    
+    -- print(Game():GetRoom():GetAwardSeed(), pickup.DropSeed)
 end)
